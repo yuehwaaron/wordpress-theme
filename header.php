e4ba00b01632c77e80c8009e0c1c16e7f9a0fb07
yuehwaaron <hsyw@me.com>
2021-01-05 22:14:08 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset')?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <?php wp_head(); ?>
-  </head> 
+  </head>  
   
   <body>
     <header class="header-area">
